6a9edc1c5af91c92c3f7d7e93ea0b0c52b2a138a
mathwqz <hs.pro.theuz@gmail.com>
2025-03-14 09:21:09 -0300

    first commit

---
+++ dblivraria.sql
@@ -0,0 +1,27 @@
+-- Banco de dados de livraria
+-- Este banco de dados será para gerenciar os livros
+
+-- Criar banco de dados
+CREATE DATABASE livraria;
+
+-- Criar tabela de livros
+CREATE TABLE livros (
+    ID_Livro INT PRIMARY KEY,
+    Titulo VARCHAR(255),
+    Autor VARCHAR(255),
+    Editora VARCHAR(255),
+    Endereço_Editora VARCHAR(255),
+    Categpria VARCHAR(255)
+);
+
+CREATE TABLE Editoras (
+ID_Editoras SERIAL PRIMARY KEY,
+Nome VARCHAR(255),
+Endereço VARCHAR(255)
+);
+ALTER TABLE livros 
+ADD ID_Editora INT REFERENCES Editoras(ID_Editoras);
+
+ALTER TABLE livros
+DROP COLUMN Editora,
+DROP COLUMN Endereço_Editora;
